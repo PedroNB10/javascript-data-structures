592e1b96f05707b7d93b416cf1d675d8a77b510b
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2024-04-10 18:10:01 -0300

    feat: add equals-operators.

---
+++ basic-javascript-types.ts
@@ -0,0 +1,42 @@
+let nullVar = null;
+let und;
+
+//console.log(nullVar); // it is used to tell that the variable doesnt have a value
+//console.log(und); // this variable wasnt attributed to any value, thats why it is undefined
+
+// bitwise operators
+
+// OR = +
+// AND = &
+// NOT = ~
+
+// console.log((1 + 0) & ~0); // ==> (1 OR 0) AND (NOT(0))
+
+// console.log(5 | 3);
+
+// console.log(5 >> 1); // deslocamento de bits para a direita
+
+let num = 2;
+let nome = "Patrick";
+let bool = true;
+let array = [1, 2, 3];
+let person = { name: "John", age: 31 };
+
+// console.log(typeof num);
+// console.log(typeof nome);
+// console.log(typeof bool);
+// console.log(typeof array);
+// console.log(typeof person);
+
+// console.log(null == undefined); // true
+// console.log(null === undefined); // false
+
+const sym1 = Symbol.for("cat");
+
+const meuObjeto = {
+  [sym1]: "João",
+};
+console.log(meuObjeto[sym1]); // João
+
+// console.log(sym1 === sym2); // false
+console.log(sym1);
